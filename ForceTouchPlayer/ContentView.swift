1a8b74315323071216b58faab5418f69eb39a904
Danilo Campana Fuchs <danilo.fuchs@ebanx.com>
2020-10-06 17:17:49 -0300

    Initial Commit

---
+++ ForceTouchPlayer/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ForceTouchPlayer
+//
+//  Created by Danilo Campana Fuchs on 06/10/20.
+//  Copyright © 2020 Danilo Campana Fuchs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
